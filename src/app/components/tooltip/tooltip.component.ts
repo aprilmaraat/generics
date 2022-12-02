1f4b6d515522748636de8b6d7b3c8b69d907d1d8
April John Maraat <aprilmaraat@gmail.com>
2022-12-02 19:25:14 +0800

    Partial tooltip progress

--- src/app/components/tooltip/tooltip.component.ts
+++ src/app/components/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-tooltip',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./tooltip.component.scss']
 })
 export class TooltipComponent {
+  @Input() text: string = 'Sample';
 
 }
